644394acc4d60c35bf8ba01c359261a6dce43e04
tuxdinosaur <ivan.tux@outlook.com>
2020-01-28 14:29:44 -0600

    [UPDATE] Version mobile terminada

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  $(".variable-width").slick({
+    fade: true,
+    arrows: true,
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 250,
+    slidesToShow: 1,
+    autoplaySpeed: 2000,
+    // centerPadding: "150px",
+    // centerMode: true,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function() {
+  $(".slick-slider").slick({});
+});
