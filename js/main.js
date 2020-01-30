1e91e008f52cb5f2a8a1fb1af110bb57bfa6f682
tuxdinosaur <ivan.tux@outlook.com>
2020-01-30 11:35:27 -0600

    [FINAL] Slider final mobile-desktop

--- js/main.js
+++ js/main.js
@@ -1,47 +1,4 @@
 $(document).ready(function() {
-  $(".variable-width").slick({
-    fade: true,
-    arrows: true,
-    dots: false,
-    infinite: true,
-    autoplay: true,
-    speed: 250,
-    slidesToShow: 1,
-    autoplaySpeed: 2000,
-    // centerPadding: "150px",
-    // centerMode: true,
-    nextArrow: $(".next"),
-    prevArrow: $(".prev"),
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: false
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-          // centerMode: false
-        }
-      }
-    ]
-  });
-});
-
-$(document).ready(function() {
   $(".slider-desktop").slick({
     fade: true,
     arrows: true,
